11ce0783a57ee2b379d5091cb9a4594fe830f114
liorkest@gmail.com <liorkest@gmail.com>
2024-08-28 22:10:48 +0300

    DONE

--- main.cpp
+++ main.cpp
@@ -1,14 +1,85 @@
 #include "customAllocator.h"
 #include <stdio.h>
-
+#include <assert.h>
 
 void print_break(const char* message) {
     void* program_break = sbrk(0);
     printf("%s: Current break is at %p\n", message, program_break);
 }
 
+void test_customMalloc() {
+    printf("\nRunning test_customMalloc...\n");
+    
+    void* ptr1 = customMalloc(32);
+    assert(ptr1 != NULL);
+    printf("Allocated 32 bytes at %p\n", ptr1);
+
+    void* ptr2 = customMalloc(64);
+    assert(ptr2 != NULL);
+    printf("Allocated 64 bytes at %p\n", ptr2);
+
+    print_list();
+}
+
+void test_customFree() {
+    printf("\nRunning test_customFree...\n");
+
+    void* ptr1 = customMalloc(32);
+    void* ptr2 = customMalloc(64);
+
+    customFree(ptr1);
+    printf("Freed memory at %p\n", ptr1);
+
+    customFree(ptr2);
+    printf("Freed memory at %p\n", ptr2);
+
+    print_list();
+}
+
+void test_merge_free_blocks() {
+    printf("\nRunning test_merge_free_blocks...\n");
+
+    void* ptr1 = customMalloc(32);
+    void* ptr2 = customMalloc(64);
+
+    customFree(ptr1);
+    customFree(ptr2);
+
+    printf("Merged adjacent free blocks\n");
+
+    print_list();
+}
+
+void test_customCalloc() {
+    printf("\nRunning test_customCalloc...\n");
+
+    void* ptr = customCalloc(4, 16);
+    assert(ptr != NULL);
+    printf("Allocated and zeroed 4 elements of 16 bytes each at %p\n", ptr);
+
+    // Verify that the allocated memory is indeed zeroed
+    unsigned char* data = (unsigned char*)ptr;
+    if(data == NULL) return;
+    for (size_t i = 0; i < 64; i++) {
+        assert(data[i] == 0);
+    }
+    print_list();
+}
+
+void test_customRealloc() {
+    printf("\nRunning test_customRealloc...\n");
+    print_list();
+    void* ptr1 = customMalloc(32);
+    print_list();
+    void* ptr2 = customRealloc(ptr1, 64);
+    assert(ptr2 != NULL);
+    printf("Reallocated memory from 32 bytes to 64 bytes at %p\n", ptr2);
+
+    print_list();
+}
+
 int main() {
-    print_break("Initial");
+        print_break("Initial");
     //printf("size of block: %lu\n", sizeof(Block));
     // Allocate some memory blocks
     void* ptr1 = customMalloc(100);
@@ -37,5 +108,15 @@ int main() {
     print_break("After free(ptr3)");
 
     customFree(ptr1);
+
+
+    test_customMalloc();
+    test_customFree();
+    test_merge_free_blocks();
+    test_customCalloc();
+    test_customRealloc();
+
+    printf("\nAll tests passed!\n");
     return 0;
 }
+
